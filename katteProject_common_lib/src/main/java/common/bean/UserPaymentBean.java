4af42c627dbce9452094dc7fb0b3c69e39f2505c
minquu <minquu@gmail.com>
2025-05-27 13:17:21 +0900

    payment 추가

---
+++ katteProject_common_lib/src/main/java/common/bean/UserPaymentBean.java
@@ -0,0 +1,12 @@
+package common.bean;
+
+import lombok.Data;
+
+@Data
+public class UserPaymentBean {
+    private int user_id;
+    private int point;
+    private int katte_money;
+    private int locked_deposit_katte_money;
+
+}
