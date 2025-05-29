1f12ab8b7bae1593472c7154d6f007054172175f
minquu <minquu@gmail.com>
2025-05-29 11:09:48 +0900

    캇체 충전 관련 로그 빈 추가

---
+++ katteProject_common_lib/src/main/java/common/bean/user/UserKatteMoneyLogBean.java
@@ -0,0 +1,18 @@
+package common.bean.user;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class UserKatteMoneyLogBean {
+    private int id;
+    private int user_id;
+    private int change_mount;
+    private reason reason;
+    private Date created_at;
+
+    public enum reason {
+        charge, user, refund
+    }
+}
