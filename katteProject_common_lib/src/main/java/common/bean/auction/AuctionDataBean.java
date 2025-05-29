fae64385ca84f9296c7fac3fb3208a5dd36dab24
jaeyeon <jydragon9@gmail.com>
2025-05-29 18:10:40 +0900

    클레스명 data 수정

--- katteProject_common_lib/src/main/java/common/bean/auction/AuctionDataBean.java
+++ katteProject_common_lib/src/main/java/common/bean/auction/AuctionDataBean.java
@@ -2,10 +2,11 @@ package common.bean.auction;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-import java.time.LocalDateTime;
+
+import java.util.Date;
 
 @Data
-public class AuctionBean {
+public class AuctionDataBean {
         @Schema(description = "경매 데이터 고유 ID", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
         private int id;
 
@@ -13,13 +14,13 @@ public class AuctionBean {
         private int product_id;
 
         @Schema(description = "경매 시작 시간 (서버에서 자동 설정)", example = "2025-05-29T12:00:00", accessMode = Schema.AccessMode.READ_ONLY)
-        private LocalDateTime auction_start_time;
+        private Date auction_start_time;
 
         @Schema(description = "판매 기간 (일 단위, 3/5/7 중 선택)", example = "5", required = true)
         private String sale_period;
 
         @Schema(description = "경매 종료 시간 (서버에서 salePeriod 기준 자동 설정)", example = "2025-06-03T12:00:00", accessMode = Schema.AccessMode.READ_ONLY)
-        private LocalDateTime auction_end_time;
+        private Date auction_end_time;
 
         @Schema(description = "최소 입찰 단위 (서버에서 currentPrice 기준 자동 설정)", example = "10000", accessMode = Schema.AccessMode.READ_ONLY)
         private int auction_insert_term;
