6c7051d57ff34fa08fb3c602ba7bd3892dbc1bc8
jeon10junho14 <19bnamfm@gmail.com>
2025-06-09 18:33:57 +0900

    bean 다시 올립니다.

---
+++ katteProject_common_lib/src/main/java/common/bean/product/ProductKatteRecommendBean.java
@@ -0,0 +1,9 @@
+package me.soldesk.katteproject_backend.test;
+
+public class ProductKatteRecommendBean {
+    private int product_id;
+    private String product_name;
+    private String brand_name;
+    private String shortform_content_url;
+    private int like_count;
+}
