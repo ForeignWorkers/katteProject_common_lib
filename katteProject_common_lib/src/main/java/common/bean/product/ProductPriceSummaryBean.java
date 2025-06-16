a2a89db453c9e6644761c39e6aa1afe88f7d6ef9
jeon10junho14 <19bnamfm@gmail.com>
2025-06-17 08:59:36 +0900

    상품상세 bean추가 및 의존성 추가

--- katteProject_common_lib/src/main/java/common/bean/product/ProductPriceSummaryBean.java
+++ katteProject_common_lib/src/main/java/common/bean/product/ProductPriceSummaryBean.java
@@ -7,6 +7,7 @@ import java.util.Date;
 
 @Data
 public class ProductPriceSummaryBean {
+
     private Integer price;               // 최근 거래가
     private Integer previous_price;      // 직전 거래가
 
@@ -16,4 +17,5 @@ public class ProductPriceSummaryBean {
     private Integer diff_amount;         // 가격 차이
     private Double diff_percent;         // 변동률
     private Integer instant_price;       // 즉시 구매 최저가
+
 }
--- katteProject_common_lib/src/main/java/common/bean/product/ProductSizeWithSellPriceBean.java
+++ katteProject_common_lib/src/main/java/common/bean/product/ProductSizeWithSellPriceBean.java
@@ -4,6 +4,8 @@ import lombok.Data;
 
 @Data
 public class ProductSizeWithSellPriceBean {
+
     private String auction_size_value;  // 예: "260", "270"
     private Integer price;              // 해당 사이즈의 최저 판매가
+
 }
