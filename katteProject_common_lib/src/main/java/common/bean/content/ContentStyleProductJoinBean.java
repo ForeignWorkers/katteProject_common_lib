21b02757e9e97ad3961ff236210285b9e8f49dab
minquu <minquu@gmail.com>
2025-06-17 11:21:31 +0900

    빈 추가

---
+++ katteProject_common_lib/src/main/java/common/bean/content/ContentStyleProductJoinBean.java
@@ -0,0 +1,10 @@
+package common.bean.content;
+
+import lombok.Data;
+
+@Data
+public class ContentStyleProductJoinBean {
+    private int id;
+    private int style_id;
+    private int product_id;
+}
